2a425036a2f64b45e304628a164b27dcab8b8505
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2026-09-15 15:37:36 -0300

    Update kitty.nix

--- modules/user/kitty.nix
+++ modules/user/kitty.nix
@@ -3,54 +3,53 @@
 {
 	programs.kitty = {
 	  enable = true;
-	  extraConfig = ''
-		font_family JetBrains Mono
-		font_size 12.5
 
-		cursor #08637d
-		cursor_shape block
+   font = {
+      size = 12;
+      name = "JetBrains Mono";
+    };
 
-		active_border_color #05c5fc
-		inactive_border_color #00181f
+    settings = {
+      copy_on_select = "yes";
+      cursor = "#08637d";
+		  cursor_shape = "block";
+			active_border_color = "#05c5fc";
+			inactive_border_color = "#00181f";
+			# Seleccion
+			selection_foreground = "#fafeff";
+			selection_background = "#015b75";
+			# Split
+			window_margin_width = 2;
+			background_tint = 0.8;
+			background_tint_gaps = -10;
+			# Tabs
+			tab_bar_edge = "top";
+			tab_bar_style = "powerline";
+			active_tab_foreground = "#fafeff";
+			active_tab_background = "#015b75";
+			inactive_tab_foreground = "#015b75";
+			inactive_tab_background = "#00181f";
+			# URLs
+			detect_urls = "yes";
+			url_style = "straight";
+			url_color = "#fafeff";
+			# The cursor shape can be one of block, beam, underline. Note that
+			cursor_beam_thickness = 1.5;
+			background_opacity = 0.75;
+			window_padding_width = 8;
+			background_blur = "1";
+    };
 
-		# Seleccion
-		selection_foreground #fafeff
-		selection_background #015b75
+    keybindings = {
+      # Split horizontal
+      "ctrl+shift+enter" = "new_window_with_cwd";
+      # Moverse entre splits con Ctrl+Shift+Flechas
+		  "ctrl+shift+left" = "neighboring_window left";
+		  "ctrl+shift+right" = "neighboring_window right";
+		  "ctrl+shift+up" = "neighboring_window up";
+		  "ctrl+shift+down" = "neighboring_window down";
+    };
 
-		# Split
-		window_margin_width 2
-		background_tint 0.8
-		background_tint_gaps -10
-
-		map ctrl+shift+enter new_window_with_cwd
-
-		# Moverse entre splits con Ctrl+Shift+Flechas
-		map ctrl+shift+left  neighboring_window left
-		map ctrl+shift+right neighboring_window right
-		map ctrl+shift+up    neighboring_window up
-		map ctrl+shift+down  neighboring_window down
-
-		# Tabs
-		tab_bar_edge top
-		tab_bar_style powerline
-		active_tab_foreground #fafeff
-		active_tab_background #015b75
-		inactive_tab_foreground #015b75
-		inactive_tab_background #00181f
-		# tab_bar_margin_color #fafeff
-
-		# URLs
-		detect_urls yes
-		url_style straight
-		url_color #fafeff
-
-		#: The cursor shape can be one of block, beam, underline. Note that
-		cursor_beam_thickness 1.5
-		background_opacity 0.75
-		window_padding_width 8
-
-		[blur]
-		background=true
-	  '';
+    shellIntegration.enableZshIntegration = true;
 	};
 }
